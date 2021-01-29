ff131b27cff42d35cf8777c3fe325079dff15b8a
tianyf <919710247@qq.com>
2021-01-29 19:55:50 +0800

    完成了重置密码的部分功能

---
+++ assets/js/users/modify-password.js
@@ -0,0 +1,34 @@
+$(function () {
+    const { form, layer } = layui
+
+    //表单校验
+    form.verify({
+        pass: [
+            /^\w{6,12}$/,
+            '密码为6到12位'
+        ],
+        confirmPass: function (val) {
+            if (val !== $('#pass').val()) {
+                return '两次密码输入不一致'
+            }
+        }
+    })
+
+    //表单提交事件
+    $('.layui-form').submit(function (e) {
+        e.preventDefault()
+
+        //发起ajax请求
+        axios.post('/my/updatepwd', $(this).serialize())
+            .then(res => {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('修改密码失败')
+                }
+                layer.msg('修改密码成功')
+            })
+    })
+
+
+
+})
