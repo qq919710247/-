2e74636d85e5d940520c480212893b3aa30c3d76
tianyf <919710247@qq.com>
2021-01-29 15:28:36 +0800

    完成了首页资料渲染功能

--- assets/js/commentAPIs.js
+++ assets/js/commentAPIs.js
@@ -4,7 +4,15 @@ axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net'
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
-    console.log('---请求之前');
+    console.log('---请求之前', config);
+
+    //获取本地存储令牌
+    const token = localStorage.getItem('token') || ''
+
+    //发送请求之间判断是否有/my开头请求的路径，如果有添加hearder请求头
+    if (config.url.startsWith('/my')) {
+        config.headers.Authorization = token
+    }
     return config;
 }, function (error) {
     // 对请求错误做些什么
@@ -15,6 +23,11 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
     console.log('---响应之前');
+    const { message, status } = response.data
+    if (message == '身份认证失败！' && status == 1) {
+        localStorage.removeItem('token')
+        location.href = './login.html'
+    }
     return response.data;
 }, function (error) {
     // 对响应错误做点什么
---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+$(function () {
+    const { layer } = layui
+
+    function getUserInfo() {
+        axios.get('/my/userinfo').then(res => {
+            console.log(res);
+            if (res.status !== 0) {
+                return layer.msg('获取用户信息失败')
+            }
+
+            const { data } = res
+            //获取用户名
+            const name = data.nickname || data.username
+            //渲染昵称
+            $('.nickname').text(`欢迎${name}`).show()
+            //渲染头像
+            if (data.user_pic) {
+                $('.avatar').prop('src', data.user_pic).show()
+                $('.text-avatar').hide()
+            } else {
+                $('.text-avatar').text(name[0].toUpperCase()).show()
+                $('.avatar').hide()
+            }
+        })
+    }
+
+    getUserInfo()
+
+    //点击退出
+    $('#logout').click(function () {
+        //清除token
+        localStorage.removeItem('token')
+
+        //跳转主页
+        location.href = './login.html'
+    })
+})
